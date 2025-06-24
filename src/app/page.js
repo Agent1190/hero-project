8173080f53c89c6ee57ec85bf15efd979443b123
Agent1190 <asghar.hammad110@gmail.com>
2025-06-24 18:33:35 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,37 @@
+import About from "@/component/About";
+import AppDownloadBanner from "@/component/AppDownloadBanner";
+import FAQPage from "@/component/FAQPage";
+import Footer from "@/component/Footer";
+import Form from "@/component/Form";
+import Hero from "@/component/Hero";
+import PricingPlan from "@/component/PricingPlan";
+import TestimonialCarousel from "@/component/TestimonialCarousel";
+
+export default function Home() {
+  return (
+    <div className=" ">
+      <Hero />
+      <section id="about" className="scroll-mt-24">
+        <About />
+      </section>
+      <div>
+        <PricingPlan />
+      </div>
+      <div>
+        <TestimonialCarousel />
+      </div>
+      <div>
+        <FAQPage />
+      </div>
+      <section id="contact" className="scroll-mt-24">
+        <Form />
+      </section>
+      <div>
+        <AppDownloadBanner />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
